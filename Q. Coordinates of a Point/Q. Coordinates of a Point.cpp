454751568b53e25bbedc5faa96af821afc44012f
modhtom <mohammedtom32@gmail.com>
2022-12-01 00:25:35 +0200

    Q Coordinates of a Point

---
+++ Q. Coordinates of a Point/Q. Coordinates of a Point.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double x,y;
+	cin >> x >> y;
+	if (x > 0 && y > 0) 
+		cout << "Q1";
+	else if (x < 0 && y < 0) 
+		cout << "Q3";
+	else if (x > 0 && y < 0) 
+		cout << "Q4";
+	else if (x < 0 && y > 0)
+		cout << "Q2";
+	 else if (x == 0 && (y > 0 || y<0))
+		cout << "Eixo Y";
+	 else if ((x < 0 || x>0) && y == 0)
+		cout << "Eixo X";
+	 else
+		cout << "Origem";
+}
